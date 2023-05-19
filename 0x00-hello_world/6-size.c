0dff423017bd4d1eaa3b3c334e12f36a0aa82ad9
Collins Manase <colloelijah@gmail.com>
2023-05-18 21:07:24 -0700

    Prints sizeof various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("size of a float: %lu byte()s", sizeof(float));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
