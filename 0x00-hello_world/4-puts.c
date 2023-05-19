6457f70ddb72eff89bfcf9be62a595eff3c96986
Collins Manase <colloelijah@gmail.com>
2023-05-18 19:58:21 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - entry point of the program
+ * puts function - prints a desired message
+ *
+ */
+
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
