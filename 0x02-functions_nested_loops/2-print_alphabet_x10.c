ffe5990720a705c67e9340b4c12a8a899140c532
Collins Manase <colloelijah@gmail.com>
2023-05-23 19:33:37 -0700

    prints 10x the aplhabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * @al - variable that takes a character
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char al;
+
+	while (i < 11)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
