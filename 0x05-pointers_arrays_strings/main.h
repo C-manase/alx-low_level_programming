eeb68ee15aa51d86bdb82f4c1023f9b155be9a0c
Collins Manase <colloelijah@gmail.com>
2023-05-30 19:05:50 -0700

    prints every other character of a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+
 int main(void);
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -9,5 +12,6 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
 
 #endif
